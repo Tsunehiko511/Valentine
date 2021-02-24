900503a90575441eb5dc26147c4d88d1871c3bc9
嶋津恒彦 <simanezumi1989@gmail.com>
2021-02-24 20:20:33 +0900

    LuaとTilemapの導入

---
+++ Assets/Scripts/GameEventManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class GameEventManager : MonoBehaviour
+{
+    [SerializeField] EventData eventData = default;
+    [SerializeField] Player player = default;
+    [SerializeField] TextAsset textAsset = default;
+
+    private void Start()
+    {
+        StartCoroutine(ExecuteEvent(textAsset.text));
+    }
+
+    IEnumerator ExecuteEvent(string script)
+    {
+        var interpreter = new LuaInterpreter(script); // スクリプトを渡して初期化
+        interpreter.AddHandler("player", player); // メッセージ制御のハンドラを登録
+        interpreter.AddHandler("event", eventData); // メッセージ制御のハンドラを登録
+        while (interpreter.HasNextScript())
+        {
+            interpreter.Resume();
+            yield return interpreter.WaitCoroutine();
+        }
+    }
+
+    IEnumerator Cor0()
+    {
+        yield return null;
+    }
+    IEnumerator Cor1()
+    {
+        yield return null;
+    }
+    IEnumerator Cor2()
+    {
+        yield return null;
+    }
+
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using MoonSharp.Interpreter;
+using UnityEngine;
+
+[MoonSharpUserData]
+public class Player : LuaInterpreterHandlerBase
+{
+    Vector3 GetDirection(string direction)
+    {
+        switch (direction)
+        {
+            default:
+                return Vector3.zero;
+            case "left":
+                return Vector3.left;
+            case "right":
+                return Vector3.right;
+            case "up":
+                return Vector3.up;
+            case "down":
+                return Vector3.down;
+        }
+    }
+
+
+    public void MoveTo(string direction, int count)
+    {
+        StartCoroutine(MoveCorou(GetDirection(direction), count));
+    }
+    public void MoveTo(Vector3 direction)
+    {
+        // StartCoroutine(MoveCorou(direction));
+    }
+
+    IEnumerator MoveCorou(Vector3 direction, int count)
+    {
+        flag = false;
+        for (int i=0; i< count; i++)
+        {
+            yield return new WaitForSeconds(0.3f);
+            transform.Translate(direction);
+        }
+        yield return new WaitForSeconds(0.5f);
+        flag = true;
+    }
+}
